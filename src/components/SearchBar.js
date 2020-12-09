c5f2ddc1d9f3debaf93a4511dd7b80f41008c412
shubham hirap <shirap07@gmail.com>
2020-12-09 10:59:45 +0530

    intial commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <>
+        <form>
+          <input type="text" />
+        </form>
+      </>
+    );
+  }
+}
+
+export default SearchBar;
